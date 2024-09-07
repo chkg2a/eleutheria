fab06c093577919cfd38906efa95ff943d10ae4e
chkg2a <xshalltearvamps@gmail.com>
2024-09-07 20:12:43 +0530

    add dummy img, fix navbar

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -9,7 +9,7 @@ import AvatarMD from "../components/smallComponents/AvatarMD";
 
 export default function NavBar({ }) {
   const user = {
-    link: `/creators/66dc00993dacb24048f6be3b`,
+    link: `/creator/66dc00993dacb24048f6be3b`,
     profilePic: "pfp1.jpg",
   };
   return (
